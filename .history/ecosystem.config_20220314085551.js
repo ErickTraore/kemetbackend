ada228bb6df13a196d89c7d315918ded374b540d
ErickTraore <traoreerick@gmail.com>
2022-03-14 09:00:08 +0100

    setup deployment-2

--- .history/ecosystem.config_20220314085551.js
+++ .history/ecosystem.config_20220314085551.js
@@ -12,7 +12,7 @@ module.exports = {
             user: 'root',
             host: 'a9e45ea.online-server.cloud',
             ref: 'origin/master',
-            repo: 'https://github.com/ErickTraore/kemetbackend.git ikcadci.eu',
+            repo: 'https://github.com/ErickTraore/kemetbackend.git ikcad',
             path: '/var/www/html',
             'pre-deploy-local': '',
             'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
