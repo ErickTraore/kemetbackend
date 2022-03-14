213e4388d8c2249da43ec742118328de7904d9ce
ErickTraore <traoreerick@gmail.com>
2022-03-14 08:50:46 +0100

    setup deployment

---
+++ .history/ecosystem.config_20220314084756.js
@@ -0,0 +1,22 @@
+module.exports = {
+    apps: [{
+        script: 'bin/www',
+        watch: '.'
+    }, {
+        script: './service-worker/',
+        watch: ['./service-worker']
+    }],
+
+    deploy: {
+        production: {
+            user: 'root',
+            host: 'a9e45ea.online-server.cloud',
+            ref: 'origin/master',
+            repo: 'GIT_REPOSITORY',
+            path: 'DESTINATION_PATH',
+            'pre-deploy-local': '',
+            'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+            'pre-setup': ''
+        }
+    }
+};
